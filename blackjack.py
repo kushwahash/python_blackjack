cc0d9557da8f6f34ba1eee90766783f3e7c307d4
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-02 02:57:40 -0600

    Added the game play logic.

--- blackjack.py
+++ blackjack.py
@@ -105,3 +105,77 @@ def hit_or_stand(deck,hand):
                 break
             else:
                 print("Not a valid choice, try again")
+
+def player_busts(player,dealer,chips):
+    print("Player is busted, bet is lost")
+    chips.loos_bet()
+
+def player_wins(player,dealer,chips):
+    print("Player Win.")
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print("Dealer is busted.Player Win.")
+    chips.win_bet()
+    
+def dealer_wins(player,dealer,chips):
+    print("Dealer Win, player looses the bet")
+    chips.loos_bet()
+    
+def push(player,dealer):
+    print("Dealer and Player Tie!!")
+
+def show_some(player,dealer):
+    print("\nDealer's Hand:")
+    print(" <card hidden>")
+    print('',dealer.cards[1])  
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    
+def show_all(player,dealer):
+    print("\nDealer's Hand:", *dealer.cards, sep='\n ')
+    print("Dealer's Hand =",dealer.value)
+    print("\nPlayer's Hand:", *player.cards, sep='\n ')
+    print("Player's Hand =",player.value)
+
+#Start the game play
+while True:
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    player_chips = Chips()
+    take_bet(player_chips)
+
+    show_some(player_hand,dealer_hand)
+
+    while playing:
+        hit_or_stand(deck,player_hand)
+        show_some(player_hand,dealer_hand)
+
+        if player_hand.value > 21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break
+    
+    if player_hand.value <= 21:
+        while dealer_hand.value < 17 :
+            hit_or_stand(deck,dealer_hand)
+            show_all(player_hand,dealer_hand)
+            if dealer_hand.value > 21:
+                dealer_busts(player_hand,dealer_hand,player_chips)
+            elif dealer_hand.value > player_hand.value:
+                dealer_wins(player_hand,dealer_hand,player_chips)
+            elif dealer_hand.value < player_hand.value:
+                player_wins(player_hand,dealer_hand,player_chips)
+            else:
+                push(player_hand,dealer_hand)
+    
+    print("Player value {}".format(player_hand.value))
+    playing = False
+

252f883c5d66bd33ce020f47fb114b1191dc7c2c
Shailesh Kushwaha <shaileshkushwaha@Shaileshs-MacBook-Pro.local>
2019-02-02 02:33:36 -0600

    Added hit_stand function

--- blackjack.py
+++ blackjack.py
@@ -8,6 +8,7 @@ suits = ('Hearts','Clubs','Spades','Diamonds')
 ranks = ('Ace','King','Queen','Jack','Ten','Nine','Eight','Seven','Six','Five','Four','Three','Two','One')
 values = {'Ace':11,'King':10,'Queen':10,'Jack':10,'Ten':10,'Nine':9,'Eight':8,'Seven':7,'Six':6,'Five':5,
 'Four':4,'Three':3,'Two':2}
+playing = True
 
 class Card:
     def __init__(self,suit,rank):
@@ -83,6 +84,27 @@ def take_bet(chips):
             else:
                 print("Your bet cannot exceed your reserve of {}".format(chips.bet))
     
+def hit(deck,hand):
+    hand.add_card(deck.deal())
+    hand.adjust_for_ace()
+
+def hit_or_stand(deck,hand):
+    global playing  # to control an upcoming while loop
+    while True:
+        try:
+            choice = input("Do you want to hit(H) or stand(S), enter your choice H or S?")
+        except:
+            print("Not a valid choice, try again")
+        else:
+            if choice[0].lower() == 'h':
+                hit(deck,hand)
+                break
+            elif choice[0].lower() == 's':
+                print("Dealers turn start......")
+                global playing = False
+                break
+            else:
+                print("Not a valid choice, try again")
 
 if __name__ == "__main__":
     #create 52 cards and add to deck
